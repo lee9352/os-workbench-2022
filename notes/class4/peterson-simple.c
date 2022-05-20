fefa49e0002d21bc41dfa5a26d6f8f6017df9752
lijue <lee9352@qq.com>
2022-05-20 15:40:00 +0800

    model-checker先研究

--- notes/class4/peterson-simple.c
+++ notes/class4/peterson-simple.c
@@ -6,6 +6,7 @@
 atomic_int nested;
 atomic_long count;
 //执行一会还是会触发assert，在于while好像判断不是原子的
+//破案了，问题不是while，而是要用barrier确保执行while之前，对标志的修改都已经写入缓存
 void critical_section() {
   long cnt = atomic_fetch_add(&count, 1);
   assert(atomic_fetch_add(&nested, 1) == 0);
@@ -18,6 +19,8 @@ void TA() {
     while (1) {
 /* PC=1 */  x = 1;
 /* PC=2 */  turn = B;
+// 加个barrier
+__sync_synchronize();
 /* PC=3 */  while (y && turn == B) ;
             critical_section();
 /* PC=4 */  x = 0;
@@ -28,6 +31,8 @@ void TB() {
   while (1) {
 /* PC=1 */  y = 1;
 /* PC=2 */  turn = A;
+// 加个barrier
+__sync_synchronize();
 /* PC=3 */  while (x && turn == A) ;
             critical_section();
 /* PC=4 */  y = 0;
