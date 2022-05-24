332e7d01ff71adbf18b0e89c9f56d2ca09e8a764
lijue <lee9352@qq.com>
2022-05-24 17:37:08 +0800

    并发编程讲解

--- notes/class6_tongbu/pc-cv.c
+++ notes/class6_tongbu/pc-cv.c
@@ -3,9 +3,11 @@
 
 int n, count = 0;
 mutex_t lk = MUTEX_INIT();
-//cond_t cv = COND_INIT();
-cond_t cv_pro = COND_INIT();
-cond_t cv_con = COND_INIT();
+cond_t cv = COND_INIT();
+// 生产者与消费者需要用不同的条件变量
+// 如果没有，会在while的条件下产生死锁
+//cond_t cv_pro = COND_INIT();
+//cond_t cv_con = COND_INIT();
 
 void Tproduce() {
   while (1) {
@@ -14,12 +16,12 @@ void Tproduce() {
     // if需要改成while
     // 目的是被唤醒之后再检查一次条件是否满足
     while (!(count < n)) {
-      //cond_wait(&cv, &lk);
-      cond_wait(&cv_pro, &lk);
+      cond_wait(&cv, &lk);
+      //cond_wait(&cv_pro, &lk);
     }
     printf("("); count++;
-    //cond_signal(&cv);
-    cond_broadcast(&cv_con);
+    cond_signal(&cv);
+    //cond_broadcast(&cv_con);
     mutex_unlock(&lk);
   }
 }
@@ -30,12 +32,12 @@ void Tconsume() {
     //if (count == 0) {
     // if需要改成while
     while (!(count > 0)) {
-      //pthread_cond_wait(&cv, &lk);
-      cond_wait(&cv_con, &lk);
+      pthread_cond_wait(&cv, &lk);
+      //cond_wait(&cv_con, &lk);
     }
     printf(")"); count--;
-    //cond_signal(&cv);
-    cond_broadcast(&cv_pro);
+    cond_signal(&cv);
+    //cond_broadcast(&cv_pro);
     mutex_unlock(&lk);
   }
 }
@@ -44,9 +46,10 @@ int main(int argc, char *argv[]) {
   assert(argc == 2);
   n = atoi(argv[1]);
   setbuf(stdout, NULL);
-  for (int i = 0; i < 8; i++) {
-    //只有一组生产者消费者时，不会报错
+  for (int i = 0; i < 1; i++) {
+    //只有一组生产者消费者时，pc-check.py不会报错
     create(Tproduce);
+    create(Tconsume);
     create(Tconsume);
   }
 }
